093a7ccaa73621eab509facd16a3d14d5da99c5b
CodesVenkat <155305281+CodesVenkat@users.noreply.github.com>
2024-01-08 08:12:49 +0530

    Create assignimginvariable.js
    assign name to image

---
+++ imgstoregetmap/Type1/assignimginvariable.js
@@ -0,0 +1,17 @@
+import PRODUCT1 from './Products/f-p-1.jpg.webp'
+import PRODUCT2 from './Products/f-p-4.jpg.webp'
+import PRODUCT3 from './Products/f-p-4.jpg.webp'
+import PRODUCT4 from './Products/f-p-5.jpg.webp'
+
+import CLOGO1 from './c-logo-1.png.webp'
+import CLOGO2 from './c-logo-2.png.webp'
+import CLOGO3 from './c-logo-3.png.webp'
+import CLOGO4 from './c-logo-4.png.webp'
+import CLOGO5 from './c-logo-5.png.webp'
+
+
+
+export const product1 = PRODUCT1;
+export const product2 = PRODUCT2;
+export const product3 = PRODUCT3;
+export const product4 = PRODUCT4;
